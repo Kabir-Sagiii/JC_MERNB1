6d1c9bb2ddf198eea2a3efbca610e3511740b68f
Kabir <sagithedeveloper@gmail.com>
2022-10-04 13:35:59 +0530

    updated the  code till 04-10-2022

---
+++ S24_04-10-2022/logicalOperator.js
@@ -0,0 +1,20 @@
+function logicO() {
+  var a = 167;
+  var x = true;
+
+  var b = 75;
+
+  var c = 68;
+
+  //   console.log(!x);
+  console.log(!(a > b));
+
+  //   console.log(a > b && a > c);
+
+  //    true && true==> true
+
+  //   console.log(a > b || a > c);
+  // f   || f = f
+}
+
+logicO();
