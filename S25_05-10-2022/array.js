2de3b1004b4b2fdbe7aecb3760248b00c74fc5b9
Kabir <sagithedeveloper@gmail.com>
2022-10-05 13:42:07 +0530

    added the data

---
+++ S25_05-10-2022/array.js
@@ -0,0 +1,35 @@
+var student = []; // empty array
+
+var emp = [101, "Darsh", "Mumbai", "darsh@gmail.com", 999999999];
+
+// console.log(emp);
+
+// console.log(emp[2]);
+
+// console.log(emp[4]);
+
+var student = [
+  101,
+  {
+    firstName: "Kabir",
+    lastName: "Sagar",
+  },
+];
+
+// console.log(student[1]);
+//  {
+//  firstName: "Kabir",
+//  lastName: "Sagar",
+//    },
+
+// console.log(student[1]["firstName"]); // Kabir
+// console.log(student[1].lastName); //Sagar
+
+var details = ["fse", { name: "kabir" }];
+
+var myObj = {
+  id: 101,
+};
+
+console.log(typeof details);
+console.log(typeof myObj);
