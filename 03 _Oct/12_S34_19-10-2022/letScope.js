3bdadaa723f2e7b12973d09d2e7a093083f4e1b6
Kabir <sagithedeveloper@gmail.com>
2022-10-19 18:47:25 +0530

    updated code till 19-10-2022

---
+++ 03 _Oct/12_S34_19-10-2022/letScope.js
@@ -0,0 +1,19 @@
+function f1() {
+  let age = 30;
+  if (true) {
+    let qty = 5;
+    var qtyNo = 100;
+    console.log(age);
+  }
+  if (true) {
+    console.log(qty);
+    if (true) {
+      if (true) {
+        let name = "kabir";
+        console.log(qtyNo);
+        console.log(age);
+      }
+      console.log(name);
+    }
+  }
+}
---
+++ 03 _Oct/12_S34_19-10-2022/varScope.js
@@ -0,0 +1,27 @@
+var marks = 899;
+
+function f1() {
+  var city = "Delhi";
+  if (true) {
+    var gender = "male";
+    console.log(city);
+  } else {
+    console.log(city);
+  }
+
+  console.log(gender);
+}
+
+f1();
+
+// function f2() {
+//     var id = 101
+//        for(){
+//          if(){
+//             if(){
+//                 var price=9999
+//             }
+//          }
+//        }
+//        console.log(price)
+// }
