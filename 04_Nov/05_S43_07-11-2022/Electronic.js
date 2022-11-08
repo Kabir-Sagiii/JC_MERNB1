42fcf6993d51e0a2cec05e733a6c613138ad95e1
Kabir <sagithedeveloper@gmail.com>
2022-11-08 14:22:05 +0530

    added code of 08-11-2022

--- 04_Nov/05_S43_07-11-2022/Electronic.js
+++ 04_Nov/05_S43_07-11-2022/Electronic.js
@@ -17,5 +17,27 @@ function getElectronicData() {
     .then(function (actualData) {
       var result = actualData;
       console.log(actualData);
+      createCard(result);
     });
+}
+
+function createCard(data) {
+  var card = document.getElementById("card");
+  data.forEach(function (ele) {
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    div.style.width = "300px";
+    div.style.height = "300px";
+    img.src = ele.image;
+    img.style.width = "100%";
+    img.height = "300";
+    div.append(img);
+    div.style.border = "2px solid black";
+    card.style.display = "grid";
+    card.style.gridGap = "10px";
+    card.style.padding = "30px";
+
+    card.style.gridTemplateColumns = "1fr 1fr 1fr";
+    card.appendChild(div);
+  });
 }
---
+++ 04_Nov/06_S44_08-11-2022/Electronic.js
@@ -0,0 +1,64 @@
+//API : https://fakestoreapi.com/products/category/electronics
+//MEthod Type : get
+
+var productId;
+function getElectronicData() {
+  // Ajax  or fetch()
+
+  var promise = fetch("https://fakestoreapi.com/products/category/electronics");
+
+  promise
+    .then(
+      function (data) {
+        //binary format //001010101010101  //json
+        return data.json();
+      },
+      function (errordata) {}
+    )
+    .then(function (actualData) {
+      //   var result = actualData;
+      console.log(actualData);
+      createCard(actualData);
+    });
+}
+
+function createCard(data) {
+  var cardRef = document.getElementById("card");
+  cardRef.style.display = "grid";
+  cardRef.style.gridTemplateColumns = "1fr 1fr 1fr";
+  cardRef.style.gridGap = "10px";
+  data.forEach(function (ele) {
+    var div = document.createElement("div");
+    var img = document.createElement("img");
+    var h4 = document.createElement("h4");
+    var p = document.createElement("p");
+    var p1 = document.createElement("p");
+    var btn = document.createElement("button");
+    var a = document.createElement("a");
+    a.href = "ProductDetails.html";
+
+    img.src = ele.image;
+    img.style.width = "100%";
+    img.style.height = "200px";
+    h4.innerText = ele.title;
+    p.innerText = "$ " + ele.price;
+    p1.innerText = ele.description;
+    btn.innerText = "More Details";
+
+    btn.style.textAlign = "center";
+    a.appendChild(btn);
+    div.appendChild(img);
+    div.appendChild(h4);
+    div.appendChild(p);
+    div.appendChild(p1);
+    div.appendChild(a);
+
+    div.style.width = "300px";
+
+    //   div.style.border = "4px solid black";
+    div.style.boxShadow = "0 0 10px black";
+    div.style.padding = "30px";
+    div.style.backgroundColor = "lightgrey";
+    cardRef.appendChild(div);
+  });
+}
