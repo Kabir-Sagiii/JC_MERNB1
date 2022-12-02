fc4940c8f1c9ed3bedeb559f20a27b5f35dd2abf
Kabir <sagithedeveloper@gmail.com>
2022-12-02 14:30:39 +0530

    aded

---
+++ 05_Dec/02_S59_02-12-2022/ReactApp/demoapp/src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React JS</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In error earum
+        odit, libero provident hic quibusdam nam corrupti praesentium dolore
+        distinctio laudantium odio eaque sapiente magni eius qui rerum. A
+        corrupti odit enim unde commodi recusandae, quisquam perspiciatis nisi
+        dicta aliquam quibusdam mollitia tenetur dolor esse debitis quod
+        blanditiis facere eveniet consequuntur, officiis, fuga itaque. Quod ex
+        velit error quam porro vero ab, sequi, at hic voluptate odit et eaque
+        laborum corrupti vitae cum nihil optio id. Suscipit commodi veniam
+        aperiam quisquam saepe ullam, recusandae, nemo molestias laborum
+        laboriosam accusamus aliquid unde ipsa nisi aspernatur quo nulla dolorum
+        placeat debitis!
+      </p>
+    </div>
+  );
+}
+
+export default App;
